810499cfe864e19a609ce6166cbba609cf0f9b61
Chathrua Gunasekara <cjgunase@mtu.edu>
2020-03-06 14:54:05 -0600

    initial commit

--- corsiv_intersect_cov.sh
+++
@@ -1,6 +0,0 @@
-for file in ./USC_pediatric_glioma_alignments/merged_CpG_eviddence/*.cov
-do
-	echo $(echo $(basename $file) | cut -f1 -d ".").bed
-	bedtools intersect -a $file -b combined_corsivs.bed -wb > ./USC_pediatric_glioma_bed_file/$(echo $(basename $file) | cut -f1 -d ".").bed
-done
-
